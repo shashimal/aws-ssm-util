02804350103a6feb93ed8aa7ad0466ca6a4c6001
Shashimal <Shashimal>
2021-09-27 15:15:31 +0800

    Updated the gitignore file

--- src/config/ssm-config.ts
+++ src/config/ssm-config.ts
@@ -1,21 +1,5 @@
-import {SsmParams} from "./ssm-params";
 
-export const SSMConfig: ISsmParams = {
-    awsAccessKeyId: "",
-    awsSecretAccessKey: "",
-    awsSessionToken: '',
-    enabled: false,
-    folder: 'demo',
-    appName: 'MyApp',
-    env: 'uat',
-    region: 'us-east-1',
-    kmsKeyId: "",
-    importParams: SsmParams.importParams,
-    exportParams: SsmParams.exportParams,
-    deleteParams: SsmParams.deleteParams
-}
-
-export interface ISsmParams {
+export interface ISsmConfig {
     region: string;
     awsAccessKeyId: string,
     awsSecretAccessKey: string;
