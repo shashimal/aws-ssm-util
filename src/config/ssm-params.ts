98229663aab0f8358d670ed7f86b3ef06bed2f21
Shashimal <Shashimal>
2021-10-01 03:10:42 +0800

    Revert "Refactored the code"
    This reverts commit a3c93f051cf641901fdecb74d1db18e7852f2689.

---
+++ src/config/ssm-params.ts
@@ -0,0 +1,10 @@
+export const SsmParams = {
+    importParams: [
+        {
+            file: 'admin.csv',
+            module: 'test'
+        },
+    ],
+    exportParams: ['/MYAPP/UAT/TEST/'],
+    deleteParams: ['/MYAPP/UAT/TEST/DB_NAME']
+}
--- src/index.ts
+++ src/index.ts
@@ -1,20 +1,16 @@
 import {SsmService} from "./ssm-service";
-import {SsmConfigUtil} from "./config/ssm-config-util";
+import {SSMConfig} from "./config/ssm-config";
 
 const main = async ()=> {
-
-    const projectName = 'Test';
-    const ssmParamsConfig  = SsmConfigUtil.getProjectConfig(projectName);
-
-    const ssmHandler = new SsmService(ssmParamsConfig);
+    const ssmHandler = new SsmService();
     //Export SSM parameters
-    await  ssmHandler.exportSSMParametersAsCSV();
+    await  ssmHandler.exportSSMParametersAsCSV(SSMConfig.exportParams!);
 
     //Import SSM parameters from csv file.
-    //await ssmHandler.importSSMParameters();
+    await ssmHandler.importSSMParameters(SSMConfig.importParams!);
 
     //Deleting SSM parameters by path
-    //await ssmHandler.deleteSSMParameters();
+    await ssmHandler.deleteSSMParameters(SSMConfig.deleteParams);
 }
 main().then(()=> "Parameters imported");
 
