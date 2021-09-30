c83f88e6d8625448209699715b0989749b288aa7
Shashimal <Shashimal>
2021-10-01 03:11:19 +0800

    Revert "Updated the gitignore file"
    This reverts commit 02804350103a6feb93ed8aa7ad0466ca6a4c6001.

--- src/config/ssm-config.ts
+++ src/config/ssm-config.ts
@@ -1,5 +1,21 @@
+import {SsmParams} from "./ssm-params";
 
-export interface ISsmConfig {
+export const SSMConfig: ISsmParams = {
+    awsAccessKeyId: "",
+    awsSecretAccessKey: "",
+    awsSessionToken: '',
+    enabled: false,
+    folder: 'demo',
+    appName: 'MyApp',
+    env: 'uat',
+    region: 'us-east-1',
+    kmsKeyId: "",
+    importParams: SsmParams.importParams,
+    exportParams: SsmParams.exportParams,
+    deleteParams: SsmParams.deleteParams
+}
+
+export interface ISsmParams {
     region: string;
     awsAccessKeyId: string,
     awsSecretAccessKey: string;
