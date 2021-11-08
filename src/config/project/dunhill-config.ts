5a94f2e63f36247ad6ca2f78e35f8e03a33118f6
Shashimal <Shashimal>
2021-11-08 20:26:27 +0800

    Dunhill config

--- src/config/project/dunhill-config.ts
+++ src/config/project/dunhill-config.ts
@@ -1,17 +1,17 @@
 import {IProjectConfig} from "../project-config";
 
-export const TangoConfig: IProjectConfig = {
+export const DunhillConfig: IProjectConfig = {
     config: [
         {
             appName: 'DUNHILL',
-            env: "DEV",
+            env: "dev",
             region: "ap-southeast-1",
-            awsAccessKeyId: "",
-            awsSecretAccessKey: "",
-            awsSessionToken: "",
+            awsAccessKeyId: "ASIA3P5W5PVLM5MX5BHN",
+            awsSecretAccessKey: "MVRq/L2DzzFOdKx9REIMzYZP5gfjDZ0xpnbU/qxO",
+            awsSessionToken: "IQoJb3JpZ2luX2VjEBQaDmFwLXNvdXRoZWFzdC0xIkgwRgIhAKX/jAdZZrIzLYJO2Kleb2dFzQPF4dfb02G0nzALSiYWAiEA71JGU1r+CB+fL8a0YWCg0/FjGr1VmsIur6jcScwhSHoqlwMIvf//////////ARAAGgw3OTAxMjA3MjU4NDYiDIdj907QN+GyIdAmASrrAmzzijmwhjHv588yUMddnH12aqqe+N1imgiRHCnPrE88/BoAZgMCaB2kn3IzI2ATIzrhlKteYKlF2aRVyF+FctC2M0BpfrmK8HUHIWYg5ZXs3XbLbk7S4dzaiuW5KqTF5EqkuNt3DUXAXYLsnulx056DQdXWiYTkSbiCV1IApHl/ua2Epx1e3BtuqeVBw7Ahk0/yPvT8B7/CYJZyvV3IB4Yqx4BQl2SVdoaU1y21eRa64IesjqMBLj52eg1hude8woZyrknXerKI+Aa+m+SVuJ7o5+EkWpG9rN454n/6pg8ES5J3B3g9EtZigNN+/aRNDsUNwtq9trZn1Ys5H9c1Lb2pDRzcGFNEI8MhRl+U2c7H9IWtLTtR54+5EhRbqTUVXfj3v2W2c3fESO01yUj0jj0ZTLCYinA/As4fztzTExO2Lyw1r/D1vzburQOvLlMyDZrn9odrldoPzv0DeYa10IG1Pj5HUjoIrn2JtzD8qaSMBjqlAdPguk7d+jT1uIf2m56UrCwCcU2ujv+nuB530utI0xy6dka86JuZCFMUIJAnBahYuGoNa+Weu7X1NXMnV6VpfxuHcLK2nm2oWeqsHVtVJZThWDIrU65pg/nDTssGxhv/bbTqalU72qpxndQhutp+/8+C9V9lCgDaulQjeSM9nHwLgMoMKC7lxXMo4BrSH4v2Cj9Xoo1apKlZJhFfHZBOsi+Jc6o5yQ==",
             kmsKeyId: "arn:aws:kms:ap-southeast-1:475226339679:key/b6126fc7-490a-4309-9d46-d1cefedf44b0",
             folder: "dunhill",
-            importFile: "improt.csv",
+            importFile: "import.csv",
             exportPath: "",
             deleteFile: ""
         },
